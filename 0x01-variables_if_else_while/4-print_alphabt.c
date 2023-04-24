cf6583cb7f349aa11b170e8130bdd2a41fcb26ee
OUMAIMA AIT TALB ALI <oumzouha@gmail>
2023-04-24 05:41:00 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "abcdfghijklmnoprstuvwxyz"
+ * main - prints the phrase "abcdfghijklmnoprstuvwxyz
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
