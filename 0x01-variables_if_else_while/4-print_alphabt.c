de8f95c50cd8a54e02489cb21432815d003b3c71
OUMAIMA AIT TALB ALI <oumzouha@gmail>
2023-04-24 05:39:16 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main - prints the phrase "abcdfghijklmnoprstuvwxyz"
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"abcdfghijklmnoprstuvwxyz");
 	return (0);
 }
