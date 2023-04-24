62d0cf8ced0ec308b3d8d74da226c2aa044a1776
OUMAIMA AIT TALB ALI <oumzouha@gmail>
2023-04-24 06:00:23 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase abcdfghijklmnoprstuvwxyz
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
 int main(void)
 {
-	puts("\ abcdfghijklmnoprstuvwxyz");
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
 	return (0);
 }
